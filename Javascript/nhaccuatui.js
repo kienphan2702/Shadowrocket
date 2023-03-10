72dd0e252a64629d1d48e803ce4271f9a1b0c0d1
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2023-03-10 15:22:00 +0000

    Update nhaccuatui.js

--- Javascript/nhaccuatui.js
+++ Javascript/nhaccuatui.js
@@ -3,7 +3,7 @@ let url = $request.url;
 const user = "users/info";
 if(url.indexOf(user) != -1)
 {
-obj.data.vipExpire= "09.09.2099";
+obj.data.vipExpire= "27.02.2099";
 obj.data.isVIP= true;
 }
 $done({body: JSON.stringify(obj)});
