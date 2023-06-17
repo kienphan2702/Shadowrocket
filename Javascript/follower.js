b68e214525c69afcf9e8c5ba7fb6e6630cedba0f
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2023-06-18 01:48:26 +0700

    Update follower.js

--- Javascript/follower.js
+++ Javascript/follower.js
@@ -7,11 +7,11 @@ objc =
     "account_id": 3744771,
     "account_auth_token": "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJqdGkiOiIzNzQ0NzcxIiwiaXNzIjoiU29jaWFsSHVlIn0.g9XnsoBFJC0gZ3w86plHHPAvz6yYeTrbazViTCTRuFk",
     "subscription": true,
-    "subscription_end": "2099-09-09T23:40:59.000000Z"
+    "subscription_end": "9999-09-09T23:40:59.000000Z"
   },
   "Action": {
     "ActionType": "toast",
-    "ActionMessage": "You are now subscribed until Jul 8, 2099"
+    "ActionMessage": "Unlocked by PTK"
   }
 }
 
