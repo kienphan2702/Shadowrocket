a37ea5b9e1d64c68fdd34967347d9cca4c5e3be5
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2023-09-05 12:20:49 +0100

    Update filmic.js

--- Javascript/filmic.js
+++ Javascript/filmic.js
@@ -1,3 +1,10 @@
+/*************************************
+APP Crack：Filmic Pro
+Phien Ban: 7.5.1
+Links: https://apps.apple.com/vn/app/filmic-pro-video-camera/id436577167?l=vi
+
+**************************************
+
 [rewrite_local]
 ^https:\/\/filmicpro\.oracle\.bendingspoonsapps\.com\/v\d\/(users\/setup|purchases\/verify) url script-response-body https://gitlab.com/ohoang7/ohb/-/raw/main/Script/filmicpro.js
 
@@ -7,7 +14,7 @@ hostname = filmicpro.oracle.bendingspoonsapps.com
 *************************************/
 
 
-var = JSON.parse($response.body);
+var ohoang7 = JSON.parse($response.body);
 
 ohoang7["me"]["active_subscriptions_ids"] = ["com.cinegenix.filmic.pro.1y_t80_1w_bis"];
 ohoang7["me"]["active_bundle_subscriptions"] = [{
@@ -17,4 +24,4 @@ ohoang7["me"]["active_bundle_subscriptions"] = [{
   }];
 ohoang7["settings"]["__identity__"]["expiration"] = "2099-09-09T09:09:09.09+00:00";
 
-$done({body : JSON.stringify});
+$done({body : JSON.stringify(ohoang7)});
