604b3e976812132b3b25a5ff142a0064b889f6a5
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-02-07 02:55:09 +0000

    Update revenuecat2.js

--- Javascript/revenuecat2.js
+++ Javascript/revenuecat2.js
@@ -146,6 +146,31 @@ const mapping = {
   'Wozi': ['wozi_pro_2023']
 };
 
+const data = {
+	"expires_date": "2099-02-18T07:52:54Z",
+	"original_purchase_date": "2020-02-11T07:52:55Z",
+	"purchase_date": "2020-02-11T07:52:54Z"
+};
+
+if (typeof $response == "undefined") {
+	delete $request.headers["x-revenuecat-etag"]; // prevent 304 issues
+	delete $request.headers["X-RevenueCat-ETag"];
+	resp.headers = $request.headers;
+} else if (obj && obj.subscriber) {
+	obj.subscriber.subscriptions = obj.subscriber.subscriptions || {};
+	obj.subscriber.entitlement = obj.subscriber.entitlement || {};
+	for (const i in list) {
+		if (new RegExp(`^${i}`, `i`).test(ua)) {
+			obj.subscriber.subscriptions[list[i].id] = data;
+			obj.subscriber.entitlements[list[i].name] = JSON.parse(JSON.stringify(data));
+			obj.subscriber.entitlements[list[i].name].product_identifier = list[i].id;
+			break;
+		}
+	}
+	resp.body = JSON.stringify(obj).replace(/\"expires_date\":\"\w{4}/g, "\"expires_date\":\"9999").replace(/\"period_type\":\"\w+\"/g, "\"period_type\":\"active\"");
+}
+$done(resp);
+
 // =========    固定部分  ========= // 
 // =========  @ddgksf2021 ========= // 
 var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],cuttlefish={Attention:"恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！",request_date_ms:1662599120248,request_date:"2022-02-27T01:05:20Z",subscriber:{non_subscriptions:{},first_seen:"2022-02-27T01:04:03Z",original_application_version:"8",other_purchases:{},management_url:"https://apps.apple.com/account/subscriptions",subscriptions:{},entitlements:{},original_purchase_date:"2022-02-27T13:05:43Z",original_app_user_id:"$RCAnonymousID:ddgksf2013",last_seen:"2022-02-27T01:04:03Z"}},ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"9999-09-09T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-02-27T01:04:18Z",purchase_date:"2022-02-27T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-02-27T01:04:17Z",product_identifier:"ddgksf2013_1y_128",expires_date:"9999-09-09T01:04:17Z"},obj=JSON.parse(JSON.stringify(cuttlefish));ddgksf2021.product_identifier="com.ddgksf2013.premium.yearly",obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013;const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s&&(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013),obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
