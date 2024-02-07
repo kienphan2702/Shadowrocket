7207e16ca005cffc1cea67bf7cf991b40b68baa7
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-02-07 02:02:01 +0000

    Update revenuecat2.js

--- Javascript/revenuecat2.js
+++ Javascript/revenuecat2.js
@@ -1,259 +1,21 @@
 /***********************************
 
-> ScriptName        RevenueCat多合一脚本[墨鱼版]
+> ScriptName        𝐑𝐞𝐯𝐞𝐧𝐮𝐞𝐂𝐚𝐭多合一脚本[墨鱼版]
 > Author            @ddgksf2013
 > ForHelp           若有屏蔽广告的需求，可公众号后台回复APP名称
 > WechatID          墨鱼手记
 > TgChannel         https://t.me/ddgksf2021
 > Contribute        https://t.me/ddgksf2013_bot
-> Feedback          📮 ddgksf2013@163.com 📮
-> UpdateTime        2024-01-25
+> Feedback          📮 𝐝𝐝𝐠𝐤𝐬𝐟𝟐𝟎𝟏𝟑@𝟏𝟔𝟑.𝐜𝐨𝐦 📮
+> UpdateTime        2024-02-02
 > Suitable          自行观看“# > ”注释内容，解锁是暂时的，购买也不是永久的[订阅、跑路]
+> Attention         📣个别失效的APP请相关需求者自行降级、或寻找替代品、或购买支持
 > Attention         如需引用请注明出处，谢谢合作！
 > ScriptURL         https://gist.githubusercontent.com/ddgksf2013/dbb1695cd96743eef18f3fac5c6fe227/raw/revenuecat.js
 
-# ========解锁列表======== #
-# > 001 白云天气
-https://apps.apple.com/cn/app/id1575901953
-# > 002 1Blocker
-https://apps.apple.com/cn/app/id1365531024
-# > 003 Aphrodite*
-https://apps.apple.com/cn/app/id1568289454
-# > 004 Apollo
-https://apps.apple.com/cn/app/id1616467801
-# > 005 pandora
-https://apps.apple.com/cn/app/id1470560916
-# > 006 widgetart
-https://apps.apple.com/cn/app/id1539097448
-# > 007 Spark
-https://apps.apple.com/cn/app/id997102246
-# > 008 Pillow
-https://apps.apple.com/cn/app/id878691772
-# > 009 图图记账*
-https://apps.apple.com/cn/app/id1546356856
-# > 010 VSCO
-https://apps.apple.com/cn/app/id588013838
-# > 011 谜底时钟
-https://apps.apple.com/cn/app/id1536358464
-# > 012 谜底黑胶
-https://apps.apple.com/cn/app/id1606306441
-# > 013 OffScreen
-https://apps.apple.com/cn/app/id1474340105
-# > 014 花样文字
-https://apps.apple.com/cn/app/id1438854446
-# > 015 ScannerPro
-https://apps.apple.com/cn/app/id333710667
-# > 016 车票票*
-https://apps.apple.com/cn/app/id6446212291
-# > 017 HTTPBot
-https://apps.apple.com/us/app/id1232603544
-# > 018 Audiomack
-https://apps.apple.com/cn/app/id921765888
-# > 019 ServerBee
-https://apps.apple.com/cn/app/id6443553714
-# > 020 NotBoring天气
-https://apps.apple.com/cn/app/id1531063436
-# > 021 NotBoring习惯
-https://apps.apple.com/cn/app/id1593891243
-# > 022 NotBoring计算器
-https://apps.apple.com/cn/app/id1533591596
-# > 023 NotBoring计时器
-https://apps.apple.com/cn/app/id1531048091
-# > 024 NotBoringVibes
-https://apps.apple.com/cn/app/id1661440185
-# > 025 倒数鸭
-https://apps.apple.com/cn/app/id6457201223
-# > 026 iptv-ultra
-https://apps.apple.com/cn/app/id1549657742
-# > 027 happy-days
-https://apps.apple.com/cn/app/id1564858029
-# > 028 chatai*[非国区旧版V3.6]
-https://apps.apple.com/us/app/id1661016696
-# > 029 aptv*[旧版V1.25]
-https://apps.apple.com/us/app/id1630403500
-# > 030 TouchRetouch
-https://apps.apple.com/cn/app/id373311252
-# > 031 方弗相机
-https://apps.apple.com/cn/app/id1621425556
-# > 032 Myjumplab
-https://apps.apple.com/us/app/id1554077178
-# > 033 目标地图
-https://apps.apple.com/cn/app/id1555022550
-# > 034 Paku
-https://apps.apple.com/cn/app/id1534130193
-# > 035 AwesomeHabits
-https://apps.apple.com/cn/app/id1514915737
-# > 036 Gear
-https://apps.apple.com/cn/app/id1458962238
-# > 037 MoneyThings
-https://apps.apple.com/cn/app/id1549694221
-# > 038 Anybox
-https://apps.apple.com/us/app/id1593408455
-# > 039 noto
-https://apps.apple.com/us/app/id1459055246
-# > 040 Widgetsmith
-https://apps.apple.com/cn/app/id1523682319
-# > 041 Percento
-https://apps.apple.com/cn/app/id1494319934
-# > 042 Planny
-https://apps.apple.com/cn/app/id1515324201
-# > 043 loopsie
-https://apps.apple.com/us/app/id1259909228
-# > 044 手机硬件管家
-https://apps.apple.com/cn/app/id1329937809
-# > 045 ImageX
-https://apps.apple.com/us/app/id1668530080
-# > 046 我的时间
-https://apps.apple.com/cn/app/id1481796842
-# > 047 Fin
-https://apps.apple.com/cn/app/id1489698531
-# > 048 星垂日记
-https://apps.apple.com/cn/app/id1663588935
-# > 049 星垂专注
-https://apps.apple.com/cn/app/id6446450915
-# > 050 Locket
-https://apps.apple.com/cn/app/id1600525061
-# > 051 one4wall
-https://apps.apple.com/us/app/id6446678464
-# > 052 mizframa
-https://apps.apple.com/cn/app/id6444951894
-# > 053 极简时钟
-https://apps.apple.com/cn/app/id1265404088
-# > 054 极简日记
-https://apps.apple.com/cn/app/id1568936702
-# > 055 治愈时钟
-https://apps.apple.com/cn/app/id1599856748
-# > 056 photomator
-https://apps.apple.com/cn/app/id1444636541
-# > 057 奇妙组件
-https://apps.apple.com/cn/app/id1466785009
-# > 058 structured
-https://apps.apple.com/cn/app/id1499198946
-# > 059 卡片馆
-https://apps.apple.com/cn/app/id1441120440
-# > 060 ColorWidgets
-https://apps.apple.com/cn/app/id1531594277
-# > 061 pdfviewer
-https://apps.apple.com/cn/app/id1120099014
-# > 062 vision
-https://apps.apple.com/us/app/id1572457968
-# > 063 photoroom
-https://apps.apple.com/us/app/id1455009060
-# > 064 pure记账
-https://apps.apple.com/cn/app/id6447791661
-# > 065 clica
-https://apps.apple.com/us/app/id1571012264
-# > 066 usage
-https://apps.apple.com/us/app/id970353453
-# > 067 TimeFinder
-https://apps.apple.com/cn/app/id1347447233
-# > 068 Brass
-https://apps.apple.com/cn/app/id1533158013
-# > 069 ElementNote
-https://apps.apple.com/cn/app/id6444015673
-# > 070 小决定
-https://apps.apple.com/cn/app/id1338769645
-# > 071 Tangerine
-https://apps.apple.com/cn/app/id1468882685
-# > 072 pastepal
-https://apps.apple.com/us/app/id1503446680
-# > 073 Airmail
-https://apps.apple.com/cn/app/id993160329
-# > 074 stresswatch
-https://apps.apple.com/us/app/id6444737095
-# > 075 IN paper
-https://apps.apple.com/cn/app/id1560313343
-# > 076 LemoFM
-https://apps.apple.com/cn/app/id6444756219
-# > 077 我的物品
-https://apps.apple.com/us/app/id1423132930
-# > 078 spektr
-https://apps.apple.com/cn/app/id1496330148
-# > 079 filmnoir
-https://apps.apple.com/cn/app/id1528417240
-# > 080 物品指南
-https://apps.apple.com/cn/app/id1585221053
-# > 081 musicmate
-https://apps.apple.com/us/app/id1605379758
-# > 082 事线
-https://apps.apple.com/cn/app/id1662454742
-# > 083 小鱼短信
-https://apps.apple.com/cn/app/id1515035333
-# > 084 Tasks
-https://apps.apple.com/cn/app/id1502903102
-# > 085 currency
-https://apps.apple.com/us/app/id284220417
-# > 086 Pixelify
-https://apps.apple.com/cn/app/id1542670372
-# > 087 小熊记账
-https://apps.apple.com/us/app/id6448206280
-# > 088 小熊轻断食
-https://apps.apple.com/cn/app/id1624214488
-# > 089 Everlog
-https://apps.apple.com/cn/app/id1519935634
-# > 090 达成21
-https://apps.apple.com/cn/app/id1568910664
-# > 091 读不舍手
-https://apps.apple.com/us/app/id1662413517
-# > 092 crystaliq
-https://apps.apple.com/us/app/id1487921214
-# > 093 FaceGlow
-https://apps.apple.com/us/app/id6466787206
-# > 094 noverdue
-https://apps.apple.com/us/app/id1492210816
-# > 095 dtdsounds
-https://apps.apple.com/us/app/id1669507883
-# > 096 dtdplaylist
-https://apps.apple.com/us/app/id1631654360
-# > 097 诗片
-https://apps.apple.com/cn/app/id1672208469
-# > 098 IntervalFlow
-https://apps.apple.com/cn/app/id6444124987
-# > 099 Routineflow
-https://apps.apple.com/cn/app/id1639800794
-# > 100 经期助手
-https://apps.apple.com/cn/app/id1032267351
-# > 101 Cookie记账
-https://apps.apple.com/cn/app/id1559943673
-# > 102 DarkNoise
-https://apps.apple.com/cn/app/id1465439395
-# > 103 计算器AI
-https://apps.apple.com/cn/app/id6462117466
-# > 104 日记
-https://apps.apple.com/us/app/id1605113008
-# > 105 Moze
-https://apps.apple.com/cn/app/id1460011387
-# > 106 AlcoholTracker
-https://apps.apple.com/cn/app/id6448653224
-# > 107 pycopaste
-https://apps.apple.com/us/app/id1635345871
-# > 108 Lilbite
-https://apps.apple.com/cn/app/id1581072935
-# > 109 Anymoney
-https://apps.apple.com/cn/app/id1310569778
-# > 110 yawn
-https://apps.apple.com/cn/app/id6473270731
-# > 111 nasctl
-https://apps.apple.com/cn/app/id1662428698
-# > 112 neurolist
-https://apps.apple.com/us/app/id6468689182
-# > 113 西江诗词
-https://apps.apple.com/us/app/id1084924739
-# > 114 geist
-https://apps.apple.com/cn/app/id897062509
-# > 115 Budgetflow
-https://apps.apple.com/us/app/id1640091876
-# > 116 Habitdone
-https://apps.apple.com/us/app/id6475660703
-# > 117 Weatherkit
-https://apps.apple.com/us/app/id1076414499
-# > 118 Timus
-https://apps.apple.com/us/app/id6461119151
-# > 119 napbot
-https://apps.apple.com/us/app/id1476436116
-# > 120 amie
-https://apps.apple.com/cn/app/id1548277133
 
+# ========解锁列表======== #
+https://appraven.net/collection/77299969
 
 [rewrite_local]
 
@@ -360,13 +122,30 @@ const mapping = {
   'Budget%20Flow': ['full_access','com.fabian.hasse.haushaltsbuch.upgrade.combined'],
   'G%20E%20I%20S%20T': ['memorado_premium'],
   'multitimer_app': ['premium','timus_lt'],
+  'Darkroom': ['co.bergen.Darkroom.entitlement.allToolsAndFilters'],
   'tiimo': ['full_access'],
+  'FaceMa/': ['Pro access'],
+  'Record2Text/': ['Pro access'],
+  'jinduoduo_calculator': ['jinduoduoapp','mobile_vip'],
   'Focused%20Work': ['Pro'],
+  'GoToSleep': ['Pro'],
+  'kegel': ['kegel_pro'],
+  'Ochi': ['Pro'],
+  'Pomodoro': ['Plus','com.MINE.PomodoroTimer.plus.yearly'],
   'Email%20Me': ['premium'],
+  'Reels%20Editor': ['Unlimited Access'],
+  'com.dison.diary': ['vip'],
+  'iRead': ['vip'],
+  'jizhi': ['jizhi_vip'],
+  'card/': ['vip'],
   'Alpenglow': ['newPro'],
-  'Darkroom': ['co.bergen.Darkroom.entitlement.allToolsAndFilters']
+  'MetadataEditor': ['unlimited_access'],
+  '%E6%9F%A5%E5%A6%86%E5%A6%86': ['Pro access'],
+  '%E5%85%83%E6%B0%94%E8%AE%A1%E6%97%B6': ['plus'],
+  'WidgetCat': ['MiaoWidgetPro'],
+  'Wozi': ['wozi_pro_2023']
 };
 
 // =========    固定部分  ========= // 
 // =========  @ddgksf2021 ========= // 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],cuttlefish={Attention:"恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！",request_date_ms:1662599120248,request_date:"2022-02-27T01:05:20Z",subscriber:{non_subscriptions:{},first_seen:"2022-02-27T01:04:03Z",original_application_version:"8",other_purchases:{},management_url:"https://apps.apple.com/account/subscriptions",subscriptions:{},entitlements:{},original_purchase_date:"2022-02-27T13:05:43Z",original_app_user_id:"$RCAnonymousID:ddgksf2013",last_seen:"2022-02-27T01:04:03Z"}},ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"2099-09-09T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-02-27T01:04:18Z",purchase_date:"2022-02-27T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-02-27T01:04:17Z",product_identifier:"ddgksf2013_1y_128",expires_date:"2099-09-09T01:04:17Z"},obj=JSON.parse(JSON.stringify(cuttlefish));ddgksf2021.product_identifier="com.ddgksf2013.premium.yearly",obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013;const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s&&(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013),obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
+var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],cuttlefish={Attention:"恭喜你抓到元数据！由墨鱼分享，请勿售卖或分享他人！",request_date_ms:1662599120248,request_date:"2022-02-27T01:05:20Z",subscriber:{non_subscriptions:{},first_seen:"2022-02-27T01:04:03Z",original_application_version:"8",other_purchases:{},management_url:"https://apps.apple.com/account/subscriptions",subscriptions:{},entitlements:{},original_purchase_date:"2022-02-27T13:05:43Z",original_app_user_id:"$RCAnonymousID:ddgksf2013",last_seen:"2022-02-27T01:04:03Z"}},ddgksf2013={is_sandbox:!1,ownership_type:"PURCHASED",billing_issues_detected_at:null,period_type:"normal",expires_date:"9999-09-09T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2022-02-27T01:04:18Z",purchase_date:"2022-02-27T01:04:17Z",store:"app_store"},ddgksf2021={grace_period_expires_date:null,purchase_date:"2022-02-27T01:04:17Z",product_identifier:"ddgksf2013_1y_128",expires_date:"9999-09-09T01:04:17Z"},obj=JSON.parse(JSON.stringify(cuttlefish));ddgksf2021.product_identifier="com.ddgksf2013.premium.yearly",obj.subscriber.subscriptions["com.ddgksf2013.premium.yearly"]=ddgksf2013;const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s&&(ddgksf2021.product_identifier=s,obj.subscriber.subscriptions[s]=ddgksf2013),obj.subscriber.entitlements[e]=ddgksf2021}else obj.subscriber.entitlements.pro=ddgksf2021;$done({body:JSON.stringify(obj)});
