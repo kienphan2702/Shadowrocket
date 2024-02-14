2bc6b441b6f591ce41d66c7e4ebb37193f83cb85
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-02-14 12:13:33 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -21,8 +21,7 @@ const data = {
 	"expires_date": "2099-09-09T07:52:54Z",
 	"original_purchase_date": "2002-02-27T07:52:55Z",
 	"purchase_date": "2020-02-27T07:52:54Z",
-	"request_date": "2020-02-27T07:52:54Z",
-	"original_purchase_date": "2002-02-27T07:52:54Z"
+	"request_date": "2020-02-27T07:52:54Z"
 };
 
 if (typeof $response == "undefined") {
