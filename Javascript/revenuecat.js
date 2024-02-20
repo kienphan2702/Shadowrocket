614724f3f4d7b7048b534f04ffc820561c30c95d
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-02-20 17:07:10 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -20,7 +20,7 @@ const list = {
 	'World Clock': { name: 'lifetime-unlock', id: 'com.overdesigned.worldclock.removeads1' },
 	'Photomator': { name: 'pixelmator_photo_pro_access', id: 'pixelmator_photo_yearly_v1' },
 	'StepsApp': { name: 'stepsapp_pedometer_premium_1_year', id: 'app.steps.stepsapp.premium.yearTrial.tier1' },
-	'PhotoRoom': { name: 'pro', id: 'com.background.pro.yearly' }
+	'PhotoRoom': { name: 'business', id: 'com.background.business.yearly' }
 };
 const data = {
 	"expires_date": "2099-09-09T07:52:54Z",
