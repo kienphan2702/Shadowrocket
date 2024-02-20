8df47633bf70647b749291779e2deff66de396bc
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-02-20 17:23:07 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -21,7 +21,8 @@ const list = {
 	'Photomator': { name: 'pixelmator_photo_pro_access', id: 'pixelmator_photo_yearly_v1' },
 	'StepsApp': { name: 'stepsapp_pedometer_premium_1_year', id: 'app.steps.stepsapp.premium.yearTrial.tier1' },
 	'PhotoRoom': { name: 'business', id: 'com.background.business.yearly' },
-	'Planny': { name: 'premium', id: 'com.kevinreutter.Sagittarius.PremiumLifetime' }
+	'Planny': { name: 'premium', id: 'com.kevinreutter.Sagittarius.PremiumLifetime' },
+	'NextMatch': { name: '$rc_lifetime', id: 'nm_199_lifetime' }
 };
 const data = {
 	"expires_date": "2099-09-09T07:52:54Z",
