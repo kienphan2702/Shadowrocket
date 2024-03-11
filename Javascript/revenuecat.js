c09ec80f697f8c542a9b11f65aee0b8cd2fd582f
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-03-11 15:53:54 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -23,7 +23,8 @@ const list = {
 	'PhotoRoom': { name: 'business', id: 'com.background.business.yearly' },
 	'Planny': { name: 'premium', id: 'com.kevinreutter.Sagittarius.PremiumLifetime' },
 	'NextMatch': { name: '$rc_lifetime', id: 'nm_199_lifetime' },
-	'Splyt': { name: 'Pro', id: 'splyt_plus_3999_1y' }
+	'Splyt': { name: 'Pro', id: 'splyt_plus_3999_1y' },
+	'Barcode': { name: 'Unlimited', id: 'com.barcodesapp.monthly' }
 };
 const data = {
 	"expires_date": "2099-09-09T07:52:54Z",
