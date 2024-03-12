81684271457cb5aa66c434969787118f4d27c4a6
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-03-12 15:16:08 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -24,7 +24,7 @@ const list = {
 	'Planny': { name: 'premium', id: 'com.kevinreutter.Sagittarius.PremiumLifetime' },
 	'NextMatch': { name: '$rc_lifetime', id: 'nm_199_lifetime' },
 	'Splyt': { name: 'Pro', id: 'splyt_plus_3999_1y' },
-	'Barcode': { name: 'Unlimited', id: 'com.barcodesapp.yearly' }
+	'Barcodes': { name: 'Unlimited', id: 'com.barcodesapp.yearly' }
 };
 const data = {
 	"expires_date": "2099-02-27T07:52:54Z",
