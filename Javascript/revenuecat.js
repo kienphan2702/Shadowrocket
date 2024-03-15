f211e172a9e5b1a4b927dd1c091ff831ee1ef1d1
Kiên Phan <91952328+kienphan2702@users.noreply.github.com>
2024-03-15 11:11:52 +0000

    Update revenuecat.js

--- Javascript/revenuecat.js
+++ Javascript/revenuecat.js
@@ -28,7 +28,7 @@ const list = {
 	'Scanner Pro': { name: 'plus', id: 'com.readdle.Scanner.subscription.year30' },
 	'Pandora': { name: '$rc_annual', id: 'com.ziheng.pandora.yearly' },
 	'Mojo': { name: 'pro', id: 'video.mojo.pro.yearly' },
-	'Color Widget': { name: '$rc_annual', id: 'cw_1999_1y_3d0' }
+	'Color Widget': { name: 'pro', id: 'cw_1999_1y_3d0' }
 };
 const data = {
 	"expires_date": "2099-09-09T07:52:54Z",
